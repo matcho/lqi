0d2c6e94c9b83e8c846cfdde342bf370ac22967a
matcho <mathias.chouet@gmail.com>
2018-01-27 14:28:51 +0100

    Ajout index dans /backoffice

---
+++ backoffice/index.php
@@ -0,0 +1,25 @@
+<?php
+	include "../pgs/entete.php";
+	include "../lib/bdd.php";
+?>
+	<body>
+		<header>
+			<div id="barremenu">
+		        <div id="logo">
+		                <img src="../img/logo/100x100.png" alt="La Quatrième Image" />
+		        </div>
+		        <div id="titre-backoffice">
+		            Backoffice
+		        </div>
+		    </div>
+		</header>
+
+		<section class="backoffice intro content candidatures">
+			<div id="content">
+				<h4><a href="actualites.php">actualités</a></h4>
+				<h4><a href="candidatures.php">candidatures</a></h4>
+				<h4><a href="newsletter.php">newsletter</a></h4>
+			</div>
+		</section>
+	</body>
+</html>
